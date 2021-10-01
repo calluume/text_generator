e4a0f92c1219d72b8634e90c6d8e76cbf571c92d
stebey <cdevans98@gmail.com>
2021-10-01 15:06:10 +0100

    Added Documentation
    - Added README file
    - Commented code

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# text_generator
- RNN text generator
+
+# RNN Text Generator
+
+A recurrent neural network text generator, created using tensorflow.
+The project includes the files for a pre-trained text generator based on 'Alice in Wonderland,'
+and a dataset of around 800,000 tweets from July and August 2020 about the US presidential election.
+
+## Model Training
+
+Each model is defined with a unique model name/ID, used for training and text generation.
+A ```banned_chars.txt``` file can be used to filter certain characters from the dataset before training.
+
+```python
+    # Create the model object with ID 'example'.
+    generator = TextGenerator('example')
+
+    # Read the dataset to the model.
+    text = generator.read_data('data/example.txt')
+
+    # Create dataset and character dictionaries.
+    generator.create_dataset(text)
+
+    # Train the model for 30 epochs.
+    generator.train_model(30)
+```
+
+## Text Generation
+
+All files needed for text generation are created during training. For a model with ID 'wonderland', these will be:
+ - ```wonderland_weights.h5```: RNN model weights.
+ - ```wonderland_model_info```: Saves model info such as vocabulary, sequence length and batch size.
+ - ```wonderland_char2int.obj``` & ```wonderland_int2char.obj```: Dictionaries for converting characters to and from integers.
+
+Text can then be generated using the ```generate_text()``` function.
+
+## Examples
+
+Generating text from the 'wonderland' model:
+```
+Seed: rFsKZACTjOAGhFjJlYWCydvXnjmYHhuPRGAhqHUOMOczxmyAyFlvNaRCQffPEOtZv
+Generated text:
+e got to
+in lyister! And I shall see if the next
+witness! No, I'll set here! No, and do nit ear leftle golden that is I used to know. Let me
+see: that it made of the glass---        OW could see's the little door!'
+```
+
+```
+Seed: TtZkTtOoTaelMuRvxBBiaqmiNctcqUsAVzEToUvIr
+Generated text:
+goon seemer legs, you know. But do cats eat bats, I wonder what Lasity that I'm going
+on she comy in lime till she'll be a presendy to seemed to be in the other
+laws.
+
+The hall, it I know she had got to things--y u inderstand will be ne time! And when I find a thing,' said the Caterpillar.
+
+'I'm afraid I can't put it more than I am
+now? That'll be a comfort, one way--near--Ho the way all store.
+```
--- textGenerator.py
+++ textGenerator.py
@@ -11,15 +11,24 @@ class TextGenerator:
         self.model_trained = False
 
     def read_data(self, filename, save_cleaned_data=False, verbose=False):
+        """
+        Reads and cleans text from the dataset.
+        :param filename:          Dataset filename
+        :param save_cleaned_data: Denotes whether to save the cleaned dataset
+        :return str:              Cleaned dataset 
+        """
+
         if verbose: print("Reading: '"+filename+"'")
 
         text = open(filename, encoding="utf-8").read()
-        banned_chars = open('data/banned_chars.txt', encoding="utf-8").read()
 
-        # Remove all links
+        # Remove all links.
         clean_text = ' '.join(word for word in text.split(' ') if not 'http' in word)
-        # Remove all banned chars
-        clean_text = clean_text.translate(str.maketrans("", "",  banned_chars))
+
+        # Remove any characters from the banned_chars file, if the file exists.
+        if os.path.exists('data/banned_chars.txt'):
+            banned_chars = open('data/banned_chars.txt', encoding="utf-8").read()
+            clean_text = clean_text.translate(str.maketrans("", "",  banned_chars))
         
         self.vocab = ''.join(sorted(set(clean_text)))
         n_unique_chars = len(self.vocab)
@@ -32,31 +41,38 @@ class TextGenerator:
         if save_cleaned_data:
             with open(filename.split('.')[0]+'_cleaned.txt', 'w') as clean_file:
                 clean_file.write(clean_text)
+
         return clean_text
 
     def create_dataset(self, text, sequence_length=100, batch_size=128, verbose=False):
-        # dictionary that converts characters to integers
+        """
+        Creates character dictionaries and builds sequences.
+        :param text: Training text
+        :param sequence_length: Sample lengths
+        :param batch_size: Training batch size
+        """
+
+        # Dictionary converting characters to integers.
         self.char2int = {c: i for i, c in enumerate(self.vocab)}
-        # dictionary that converts integers to characters
+        # Dictionary converting integers to characters.
         self.int2char = {i: c for i, c in enumerate(self.vocab)}
 
-        # save these dictionaries for later generation
         pickle.dump(self.char2int, open("obj/"+self.model_name+"_char2int.obj", "wb"))
         pickle.dump(self.int2char, open("obj/"+self.model_name+"_int2char.obj", "wb"))
 
-        # convert all text into integers
+        # Convert all text into integers.
         self.encoded_text = np.array([self.char2int[c] for c in text])
-        # construct tf.data.Dataset object
+        # Construct tf.data.Dataset object.
         char_dataset = tf.data.Dataset.from_tensor_slices(self.encoded_text)
         
-        # build sequences by batching
+        # Build sequences by batching.
         self.sequence_length = sequence_length
         self.batch_size = batch_size
         sequences = char_dataset.batch(2*self.sequence_length + 1, drop_remainder=True)
 
         self.dataset = sequences.flat_map(self.split_sample).map(self.one_hot_samples)
 
-        # print first 2 samples
+        # Prints the first 2 samples.
         if verbose:
             for element in self.dataset.take(2):
                 print("Input:", ''.join([self.int2char[np.argmax(char_vector)] for char_vector in element[0].numpy()]))
@@ -65,23 +81,29 @@ class TextGenerator:
                 print("Target shape:", element[1].shape)
                 print("="*50, "\n")
 
-        # repeat, shuffle and batch the dataset
+        # Repeats, shuffles and batches the dataset.
         self.dataset = self.dataset.repeat().shuffle(1024).batch(self.batch_size, drop_remainder=True)
 
     def train_model(self, epochs):
+        """
+        Trains the RNN model.
+        :param epochs: Number of epochs to train for
+        """
+
         self.create_model()
 
         self.model.summary()
         self.model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-        # make results folder if does not exist yet
+        # Make results folder if does not exist yet.
         if not os.path.isdir("results"):
             os.mkdir("results")
-        # train the model
+
+        # Train and save the model.
         self.model.fit(self.dataset, steps_per_epoch=(len(self.encoded_text) - self.sequence_length) // self.batch_size, epochs=epochs)
-        # save the model
         self.model.save("results/"+self.model_name+"_weights.h5")
 
+        # Saves model information as a json file for text generation.
         model_info = {'modelName':      self.model_name,
                       'vocab':          self.vocab,
                       'sequenceLength': self.sequence_length,
@@ -90,10 +112,15 @@ class TextGenerator:
         with open('data/'+self.model_name+'_model_info.json', 'w') as json_file:
             json.dump(model_info, json_file, indent=4)
 
+        # Tells the model that it doesn't need to load model data
+        # from local files.
         self.model_trained = True
 
     def create_model(self):
-        # a better model (slower to train obviously)
+        """
+        Defines a model for use in training and text generation.
+        """
+
         self.model = Sequential([
             LSTM(256, input_shape=(self.sequence_length, len(self.vocab)), return_sequences=True),
             Dropout(0.3),
@@ -102,12 +129,18 @@ class TextGenerator:
         ])
 
     def split_sample(self, sample):
+        """
+        Splits and encodes samples.
+        :param sample: Text sample to split
+        :return [str]: Input text and target characters
+        """
+
         # example :
         # sequence_length is 10
         # sample is "python is a great pro" (21 length)
         # ds will equal to ('python is ', 'a') encoded as integers
-        
         ds = tf.data.Dataset.from_tensors((sample[:self.sequence_length], sample[self.sequence_length]))
+
         for i in range(1, (len(sample)-1) // 2):
             # first (input_, target) will be ('ython is a', ' ')
             # second (input_, target) will be ('thon is a ', 'g')
@@ -118,21 +151,40 @@ class TextGenerator:
             # extend the dataset with these samples by concatenate() method
             other_ds = tf.data.Dataset.from_tensors((input_, target))
             ds = ds.concatenate(other_ds)
+
         return ds
 
     def one_hot_samples(self, input_, target):
-        # onehot encode the inputs and the targets
+        """
+        Encodes the inputs and targets asbinary vectors
+        :param input:  Sample
+        :param target: Target variable
+        :return [0|1]: Encoded variable
+        """
+
         # Example:
         # if character 'd' is encoded as 3 and n_unique_chars = 5
         # result should be the vector: [0, 0, 0, 1, 0], since 'd' is the 4th character
         return tf.one_hot(input_, len(self.vocab)), tf.one_hot(target, len(self.vocab))
 
-    def generate_text(self, seed, n_sentences=1, max_chars=400, model_file='example.json'):
+    def generate_text(self, seed, n_sentences=1, max_chars=400, model_file=None):
+        """
+        Generates text from trained weights.
+        :param seed:        Generation seed
+        :param n_sentences: Number of sentences to generate
+        :param max_chars:   Maximum number of characters
+        :param model_file:  Location of model information file
+        :return str:        Generated text
+        """
 
+        if model_file == None:
+            model_file = self.model_name+'_model_info.json'
+
+        # If the model has not been trained during this execution,
+        # all necessary data is defined from local files.
         if not self.model_trained:
             with open('data/'+model_file) as json_file:
                 model_info = json.load(json_file)
-                self.model_name = model_info['modelName']
                 self.vocab = model_info['vocab']
                 self.sequence_length = model_info['sequenceLength']
                 self.batch_size = model_info['batchSize']
@@ -143,37 +195,43 @@ class TextGenerator:
 
         vocab_size = len(self.char2int)
 
-        # load the optimal weights
+        # Loads the trained weights.
         self.model.load_weights("results/"+self.model_name+"_weights.h5")
 
         generated = ""
         original_seed = seed
         for i in tqdm.tqdm(range(max_chars), "Generating text"):
-            # make the input sequence
+            # Create the input sequence.
             X = np.zeros((1, self.sequence_length, len(self.vocab)))
             for t, char in enumerate(seed):
                 X[0, (self.sequence_length - len(seed)) + t, self.char2int[char]] = 1
-            # predict the next character
+            # Predicts the next character.
             predicted = self.model.predict(X, verbose=0)[0]
-            # converting the vector to an integer
+            # Converting the vector to an integer.
             next_index = np.argmax(predicted)
-            # converting the integer to a character
+            # Converting the integer to a character.
             next_char = self.int2char[next_index]
-            # add the character to results
+            # Adding the character to results.
             generated += next_char
-            # shift seed and the predicted character
+            # Shifts seed and the predicted character.
             seed = seed[1:] + next_char
 
         print("Seed:", original_seed)
         print("Generated text:")
         print(generated)
+
         return generated
 
 if __name__ == "__main__":
-    model = 'tweets' # or wonderland
+    # Define the model.
+    model = 'wonderland' # 'tweets' or 'wonderland'
     generator = TextGenerator(model)
-    text = generator.read_data('data/'+model+'.txt', True, verbose=True)
-    generator.create_dataset(text, verbose=True)
-    generator.train_model(30)
+
+    # Create the dataset and train the model.
+    #text = generator.read_data('data/'+model+'.txt', True, verbose=True)
+    #generator.create_dataset(text, verbose=True)
+    #generator.train_model(30)
+
+    # Generates text.
     random_seed = ''.join(random.choice(string.ascii_letters) for i in range(random.randint(5, 100)))
-    generated_text = generator.generate_text(random_seed, max_chars=400, model_file=model+'_model_info.json')
+    generated_text = generator.generate_text(random_seed)
