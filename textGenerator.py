c54efc2162ed8362407ef98ed33e59352620027a
calluume <cdevans98@gmail.com>
2022-11-15 19:00:57 +0000

    Added ability to load previous weights
    If a model is already trained, the weights can be loaded again to train further

--- textGenerator.py
+++ textGenerator.py
@@ -1,22 +1,31 @@
 import tensorflow as tf
 import numpy as np
 import pickle, tqdm, os, json
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 
 class TextGenerator:
-    def __init__(self, model_name):
+    def __init__(self, model_name: str):
         self.model_name = model_name
         self.model_file = 'models/'+model_name
-        self.model_trained = False
 
-        if not os.path.exists('models/'+model_name):
+        if not os.path.exists(self.model_file):
             os.makedirs(self.model_file)
             os.makedirs(self.model_file+'/obj')
             os.makedirs(self.model_file+'/data')
             os.makedirs(self.model_file+'/results')
 
-    def read_data(self, dataset_file=None, banned_chars_file=None, save_cleaned_data=False, verbose=False):
+            self.model_trained = False
+
+        elif os.path.exists(self.model_file+'/results/model_info.json'):
+            with open(self.model_file+'/results/model_info.json', 'r') as info_file:
+                model_info = json.load(info_file)
+                self.vocab = model_info["vocab"]
+                self.sequence_length = model_info["sequenceLength"]
+                self.batch_size = model_info["batchSize"]
+                self.model_trained = model_info["trained"]
+
+    def read_data(self, dataset_file: str = None, banned_chars_file: str = None, save_cleaned_data: bool = False, verbose: bool = False):
         """
         Reads and cleans text from the dataset.
         :param dataset_file:      Dataset filename
@@ -25,6 +34,7 @@ class TextGenerator:
         :return str:              Cleaned dataset 
         """
 
+        # change to default to clean_dataset.txt if exists
         if dataset_file == None:
             dataset_file = self.model_file+'/data/dataset.txt'
 
@@ -57,7 +67,7 @@ class TextGenerator:
 
         return clean_text
 
-    def create_dataset(self, text, sequence_length=100, batch_size=128, verbose=False):
+    def create_dataset(self, text: str, sequence_length: int = 100, batch_size: int = 128, verbose: bool = False):
         """
         Creates character dictionaries and builds sequences.
         :param text: Training text
@@ -94,8 +104,9 @@ class TextGenerator:
 
         # Repeats, shuffles and batches the dataset.
         self.dataset = self.dataset.repeat().shuffle(1024).batch(self.batch_size, drop_remainder=True)
+        self.save_model_info()
 
-    def train_model(self, epochs):
+    def train_model(self, epochs: int):
         """
         Trains the RNN model.
         :param epochs: Number of epochs to train for
@@ -108,31 +119,27 @@ class TextGenerator:
 
         # Train and save the model.
         self.model.fit(self.dataset, steps_per_epoch=(len(self.encoded_text) - self.sequence_length) // self.batch_size, epochs=epochs)
-        self.model.save(self.model_file+"results/weights.h5")
+        self.model.save(self.model_file+"/results/weights.h5")
 
-        # Saves model information in a json file for text generation.
-        model_info = {'modelName':      self.model_name,
-                      'vocab':          self.vocab,
-                      'sequenceLength': self.sequence_length,
-                      'batchSize':      self.batch_size}
-
-        with open(self.model_file+'/results/model_info.json', 'w') as json_file:
-            json.dump(model_info, json_file, indent=4)
-
-        # Tells the model that it doesn't need to load model data from local files.
+        # Save model information
         self.model_trained = True
+        self.save_model_info()
 
     def create_model(self):
         """
         Defines a model for use in training and text generation.
         """
 
-        self.model = Sequential([
-            LSTM(256, input_shape=(self.sequence_length, len(self.vocab)), return_sequences=True),
-            Dropout(0.3),
-            LSTM(256),
-            Dense(len(self.vocab), activation="softmax"),
-        ])
+        if self.model_trained:
+            self.model = load_model(self.model_file+'/results/weights.h5')
+
+        else:
+            self.model = Sequential([
+                LSTM(256, input_shape=(self.sequence_length, len(self.vocab)), return_sequences=True),
+                Dropout(0.2),
+                LSTM(256),
+                Dense(len(self.vocab), activation="softmax"),
+            ])
 
     def split_sample(self, sample):
         """
@@ -158,13 +165,13 @@ class TextGenerator:
         :param target: Target variable
         :return [0|1]: Encoded variable
         """
-
+        
         # Example:
         # if character 'd' is encoded as 3 and n_unique_chars = 5
         # result should be the vector: [0, 0, 0, 1, 0], since 'd' is the 4th character
         return tf.one_hot(input_, len(self.vocab)), tf.one_hot(target, len(self.vocab))
 
-    def generate_text(self, seed, n_sentences=1, max_chars=400, stop_char=None, model_info_filepath=None, verbose=True):
+    def generate_text(self, seed: str, max_chars: int = 200, max_sentences: int = 1, stop_char: str = None, verbose: bool = True):
         """
         Generates text from trained weights.
         :param seed:                Generation seed
@@ -175,46 +182,46 @@ class TextGenerator:
         :return str:                Generated text
         """
 
-        if model_info_filepath == None:
-            model_info_filepath = self.model_file+'/results/model_info.json'
-
+        """
         # If the model has not been trained during this execution,
         # all necessary data is defined from local files.
         if not self.model_trained:
-            with open(model_info_filepath) as json_file:
+            with open(self.model_file+'/results/model_info.json') as json_file:
                 model_info = json.load(json_file)
                 self.vocab = model_info['vocab']
                 self.sequence_length = model_info['sequenceLength']
                 self.batch_size = model_info['batchSize']
+        """
 
-            self.char2int = pickle.load(open(self.model_file+'/obj/char2int.obj', "rb"))
-            self.int2char = pickle.load(open(self.model_file+'/obj/int2char.obj', "rb"))
-            self.create_model()
-
-        vocab_size = len(self.char2int)
+        self.char2int = pickle.load(open(self.model_file+'/obj/char2int.obj', "rb"))
+        self.int2char = pickle.load(open(self.model_file+'/obj/int2char.obj', "rb"))
+        self.create_model()
 
         # Loads the trained weights.
         self.model.load_weights(self.model_file+"/results/weights.h5")
 
+        n_sentences = 0
         generated = ""
         original_seed = seed
         for _ in tqdm.tqdm(range(max_chars), "Generating text"):
+
             # Create the input sequence.
             X = np.zeros((1, self.sequence_length, len(self.vocab)))
             for t, char in enumerate(seed):
                 X[0, (self.sequence_length - len(seed)) + t, self.char2int[char]] = 1
+
             # Predicts the next character.
             predicted = self.model.predict(X, verbose=0)[0]
-            # Converting the vector to an integer.
             next_index = np.argmax(predicted)
-            # Converting the integer to a character.
             next_char = self.int2char[next_index]
-            # Adding the character to results.
+
+            if len(generated) > 0 and next_char == ' ' and generated[-1] in ".!?()[]'\"": n_sentences += 1
+
+            # Add the character to results and shift the seed.
             generated += next_char
-            # Shifts seed and the predicted character.
             seed = seed[1:] + next_char
 
-            if stop_char != None and next_char == stop_char:
+            if (stop_char != None and next_char == stop_char) or (n_sentences == max_sentences):
                 break
 
         if verbose:
@@ -223,3 +230,18 @@ class TextGenerator:
             print(generated)
 
         return generated
+
+    def save_model_info(self):
+        """
+        Saves model information for training.
+        """
+
+        model_info = {'modelName':      self.model_name,
+                      'vocab':          self.vocab,
+                      'sequenceLength': self.sequence_length,
+                      'batchSize':      self.batch_size,
+                      'trained':        self.model_trained
+                     }
+
+        with open(self.model_file+'/results/model_info.json', 'w') as json_file:
+            json.dump(model_info, json_file, indent=4)
